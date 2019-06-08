a83f697762616eaa9c75f0fe16c0ef9f675011b0
ggilley <ggilley@gerg.org>
2019-06-08 12:03:35 -0700

    Update 1.3.2.sh
    Adjust test

--- test/1.3.2.sh
+++ test/1.3.2.sh
@@ -3,8 +3,4 @@
 
 # 1.3.2 - Ensure filesystem integrity is regularly checked (Scored)
 
-if [[ $(ls -A /etc/cron.*) ]] ; then
- grep -r aide /etc/cron.* /etc/crontab || exit $?
-else
- crontab -u root -l | grep aide || exit $?
-fi
+crontab -u root -l | grep aide || exit $?
